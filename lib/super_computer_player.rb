8f1ab00fa6ab04cc46c5dde30f2bdfe3ca93cd44
sam-kim99 <samkim0451@gmail.com>
2024-01-29 17:15:16 -0500

    define super_computer_player.rb

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -1,7 +1,16 @@
 require_relative 'tic_tac_toe_node'
+require 'byebug'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    # debugger
+    node = TicTacToeNode.new(game.board, mark)
+    children = node.children
+    winner_children = children.filter {|child| child.winning_node?(mark)}
+    winner_children += children.reject {|child| child.losing_node?(mark)}
+    winner = winner_children.first
+    raise "unwinnable game" if winner == nil
+    winner.prev_move_pos
   end
 end
 
